c643b4ca1161b7a0dbbdb460da233e7226334562
jotaDEVe <joaovitordok@gmail.com>
2024-06-19 22:45:16 -0300

    Exercícios de matrizes

---
+++ Matrizes/ExercicioMatrizesQuadradas/Program.cs
@@ -0,0 +1,48 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Insira o tamanho da matriz quadrada (linhas = colunas): ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[,] matriz = new int[n, n];
+
+
+        Console.WriteLine("Entre com os valores separados por espaço: ");
+        //Itera sobre as linhas
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            string[] values = Console.ReadLine().Split(' ');
+
+            //Itera sobre as colunas
+            for (int j = 0; j < n; j++)
+            {
+                matriz[i, j] = int.Parse(values[j]);
+            }
+        }
+
+        //Imprime a Diagonal Principal
+        Console.WriteLine();
+        Console.WriteLine("Diagonal Principal:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(matriz[i, i] + " ");
+        }
+        Console.WriteLine();
+
+        //Conta todos os números negativos
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        { 
+            for(int j = 0;j < n; j++)
+            {
+                if (matriz[i,j] < 0)
+                {
+                    count++;
+                }
+            }
+        }
+        Console.WriteLine("Existem " + count + " números negativos na matriz percorrida");
+    }
+}
---
+++ Matrizes/ExercícioIndicesMatrizes/PesquisaIndices/Program.cs
@@ -0,0 +1,71 @@
+﻿
+Console.WriteLine("Entre com o número de linhas da matriz: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Entre com o número de colunas da matriz: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[m, n];
+
+
+Console.WriteLine("Entre com os valores separados por espaços: ");
+
+
+//Itera sobre as linhas
+for (int i = 0; i < m; i++)
+{
+   String[] values = Console.ReadLine().Split(" ");
+
+    //Itera sobre as colunas
+    for(int j = 0; j < n; j++)
+    {
+        matriz[i, j] = int.Parse(values[j]);
+    }
+
+}
+
+Console.WriteLine("\nDigite um número da matriz para ler suas coordenadas: ");
+int numAlvo = int.Parse(Console.ReadLine());
+
+int linhaNumAlvo = 0;
+int colunaNumAlvo = 0;
+
+//Itera sobre as linhas
+for (int i = 0; i < m; i++)
+{
+
+    //Itera sobre as colunas
+    for (int j = 0; j < n; j++)
+    {
+        if (matriz[i,j] == numAlvo)
+        {
+            linhaNumAlvo = i;
+            colunaNumAlvo = j;
+            break;
+        }
+    }
+
+}
+
+Console.WriteLine("\n#########################################");
+Console.WriteLine("Posição do número: [" + linhaNumAlvo + ", " + colunaNumAlvo + "]");
+
+if (colunaNumAlvo > 0) 
+{
+    Console.WriteLine("Esquerda do número: " + matriz[linhaNumAlvo, colunaNumAlvo - 1]);
+}
+
+if(colunaNumAlvo < n - 1)
+{
+    Console.WriteLine("Direita do número: " + matriz[linhaNumAlvo, colunaNumAlvo + 1]);
+}
+
+if(linhaNumAlvo < m - 1)
+{
+    Console.WriteLine("Abaixo do número: " + matriz[linhaNumAlvo + 1, colunaNumAlvo]);
+}
+
+if (linhaNumAlvo > 0)
+{
+    Console.WriteLine("Acima do número: " + matriz[linhaNumAlvo - 1, colunaNumAlvo]);
+}
+Console.WriteLine("##########################################");
